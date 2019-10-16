585f43e6e21f7ea5ebbe545efdb05fbcdef2ce71
Tao Long <iamtaolong@gmail.com>
2019-10-16 19:19:00 -0400

    [Tao Long] Question#1 First Commit

--- java/src/main/java/skeleton/answers/Question1.java
+++ java/src/main/java/skeleton/answers/Question1.java
@@ -2,9 +2,27 @@ package skeleton.answers;
 
 public class Question1 {
 
+
     public static double calculateTotalPayment(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage) {
         // TODO Auto-generated method stub
+
+        double monthlyPayment = repaymentPercentage * initialLevelOfDebt;
+        double currentTotal = initialLevelOfDebt;
+        int month = 0;
+
+        while (currentTotal < monthlyPayment){
+            currentTotal *= (1 + interestPercentage);
+            currentTotal -= monthlyPayment;
+            month++;
+
+        }
+
+        double finalPayment = currentTotal;
+        double overallPayment = month * monthlyPayment + (0.1 * initialLevelOfDebt) + finalPayment;
+        System.out.print("Sum of papyments: " + overallPayment);
+
         return -5;
     }
+
 
 }
